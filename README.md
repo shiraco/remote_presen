56c3d785befce8d986b79425f6035a5f39f9e0c9
Koji Shiraishi <shiraco@gmail.com>
2015-08-10 23:00:14 +0900

    Apply qimessaging

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ https://remote-presen.herokuapp.com/
 [![Build Status](https://travis-ci.org/shiraco/remote_presen.svg?branch=master)](https://travis-ci.org/shiraco/remote_presen)
 
 [![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+## References
+* https://github.com/kuetsuhara/kuetsuhara.github.io
+* https://github.com/haiatto/pepper_for_qiita
+* http://remarkjs.com/
---
+++ static/robot.js
@@ -0,0 +1,29 @@
+function sampleButtonClicked() {
+    session.service("ALMemory").done(function (ALMemory) {
+        console.log("ALMemory取得成功");
+        ALMemory.raiseEvent("PepperQiMessaging/fromtablet", "押したね");
+    });
+}
+
+function connect() {
+
+    var robotIp = $("#ip1").val() + "." + $("#ip2").val() + "." + $("#ip3").val() + "." + $("#ip4").val();
+
+    self.qims = new QiSession(robotIp);
+	  self.qims.socket()
+        .on('connect', function () {
+            console.log("connected");
+       	 		    self.qims.service("ALTextToSpeech").done(function (tts) {
+       	 	          tts.say("接続");
+       	 	      });
+            })
+        .on('disconnect', function () {
+            console.log("disconnected");
+        });
+}
+
+function say() {
+    console.log("say");
+	  var value = $("#sayText").val();
+	  this.alTextToSpeech.say(value);
+}
